c85dc03675cc38ff907d4d99371a8816d5371ecf
vas1levnv <vas1levnv@yandex.ru>
2023-01-13 14:01:27 +0300

    свайпер

--- public_html/assets/script.js
+++ public_html/assets/script.js
@@ -5,4 +5,42 @@ $(document).ready(function() {
 		$('.header-menu').toggleClass('active')
 	})
 	
+	
+	$('#shops').on('click', function(){
+		$('#shops').addClass('active')
+		$('#swiper-shops').addClass('active')
+		$('#food').removeClass('active')
+		$('#swiper-food').removeClass('active')
+		$('#entertainment').removeClass('active')
+		$('#swiper-entertainment').removeClass('active')
+	})
+	
+	$('#food').on('click', function(){
+		$('#food').addClass('active')
+		$('#swiper-food').addClass('active')
+		$('#swiper-shops').removeClass('active')
+		$('#shops').removeClass('active')
+		$('#entertainment').removeClass('active')
+		$('#swiper-entertainment').removeClass('active')
+	})
+	
+	$('#entertainment').on('click', function(){
+		$('#entertainment').addClass('active')
+		$('#swiper-entertainment').addClass('active')
+		$('#shops').removeClass('active')
+		$('#swiper-shops').removeClass('active')
+		$('#food').removeClass('active')
+		$('#swiper-food').removeClass('active')
+	})
+	
+	const swiper = new Swiper('.swiper', {
+		slidesPerView: 2.2,
+		spaceBetween: 6,
+		loop: true,
+		breakpoints:{
+			768:{
+				slidesPerView: 1.5,
+			}
+		}
+	})
 })
